93b4787df96704864d7f244b42e2c329fc818d5c
Artem Egorof <areg@goldcarrot.ru>
2023-12-13 13:55:50 +0300

    fourth commit

--- src/js/modules/mobile-nav.js
+++ src/js/modules/mobile-nav.js
@@ -1,18 +1,24 @@
 function mobileNav() {
 	const navBtnOpen = document.querySelector("#modalOpen");
-	const navBtnClose = document.querySelector("#modalClose");
+	const checkbox = document.querySelector("#yourCheckboxId");
 
 	const navOverlay = document.querySelector("#mobileNavOverlay");
 	const nav = document.querySelector("#mobileNav");
 
 	navBtnOpen.onclick = toggleMobileNav;
-	navBtnClose.onclick = toggleMobileNav;
+	// navBtnClose.onclick = toggleMobileNav;
 	navOverlay.onclick = toggleMobileNav;
 
-	function toggleMobileNav() {
+	function toggleMobileNav(event) {
 		navOverlay.classList.toggle("mobile-nav-overlay--open");
 		nav.classList.toggle("mobile-nav--open");
 		document.body.classList.toggle("no-scroll");
+
+		// Проверяем, было ли событие вызвано кликом по оверлею
+		if (event.target === navOverlay) {
+			// Сбрасываем состояние чекбокса
+			checkbox.checked = false;
+		}
 	}
 }
 
